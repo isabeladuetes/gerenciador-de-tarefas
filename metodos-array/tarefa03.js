70f21c0aef2b91310a074bddd6a2871e6f23b81c
isabeladuetes <isabela.duetes@aluno.senai.br>
2025-05-30 09:08:22 -0300

    feat: Adiciona tarefa no início

---
+++ metodos-array/tarefa03.js
@@ -0,0 +1,10 @@
+let tarefas = ['Estudar JavaScript', 'Fazer exercícios de matemática', 'Lavar a louça', 'Comprar mercado', 'Ir à academia', 'Ler um livro', 'Fazer revisão para o teste', 'Limpar o quarto', 'Estudar para concurso', 'Organizar o computador'];
+
+console.log("Antes do unshift:")
+console.table(tarefas);
+
+// Adicionando uma nova tarefa no início do array
+tarefas.unshift('Dançar Just Dance');
+
+console.log("Depois do unshift:");
+console.table(tarefas);
