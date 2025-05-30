03ec872905f1071ed64de78d98d24abee0241f8b
isabeladuetes <isabela.duetes@aluno.senai.br>
2025-05-30 11:12:33 -0300

    feat: Inverte ordem das tarefas

---
+++ metodos-array/tarefa13.js
@@ -0,0 +1,6 @@
+let tarefas = ['Estudar JavaScript', 'Fazer exercícios de matemática', 'Lavar a louça', 'Comprar mercado', 'Ir à academia', 'Ler um livro', 'Fazer revisão para o teste', 'Limpar o quarto', 'Estudar para concurso', 'Organizar o computador'];
+
+//Invertendo a ordem das tarefas
+let tarefasInvertidas = tarefas.reverse();
+
+console.log("Tarefas invertidas:", tarefasInvertidas);
