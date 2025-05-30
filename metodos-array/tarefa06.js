7a3c6ab36d0f46acf7f589c091b48b99bf3e8884
isabeladuetes <isabela.duetes@aluno.senai.br>
2025-05-30 09:49:10 -0300

    feat: Transfor a lista de tarefas em string

---
+++ metodos-array/tarefa06.js
@@ -0,0 +1,4 @@
+let tarefas = ['Estudar JavaScript', 'Fazer exercícios de matemática', 'Lavar a louça', 'Comprar mercado', 'Ir à academia', 'Ler um livro', 'Fazer revisão para o teste', 'Limpar o quarto', 'Estudar para concurso', 'Organizar o computador'];
+
+//Transformando a lista de tarefas em uma string separada por vírgulas
+console.log("Lista de tarefas com string:", tarefas.join(', ')); 
