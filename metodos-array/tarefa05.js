1b15cf01da3a74bbcbe89ea1d2b67db9fbdd4960
isabeladuetes <isabela.duetes@aluno.senai.br>
2025-05-30 09:43:30 -0300

    refactor: Edite um elemento

--- metodos-array/tarefa05.js
+++ metodos-array/tarefa05.js
@@ -1,5 +1,6 @@
 let tarefas = ['Estudar JavaScript', 'Fazer exercícios de matemática', 'Lavar a louça', 'Comprar mercado', 'Ir à academia', 'Ler um livro', 'Fazer revisão para o teste', 'Limpar o quarto', 'Estudar para concurso', 'Organizar o computador'];
 
+//Verificando se a tarefa "Ir à academia" está na lista e se não estiver, adicionando
 if (tarefas.includes("Ir à academia")) {
     console.log("A lista já inclui essa tarefa: Ir à academia");
 } else {
