444a5cdc7996ad34d07bc2e47c602c137e2799a9
isabeladuetes <isabela.duetes@aluno.senai.br>
2025-05-30 10:06:33 -0300

    feat: Altera lista

---
+++ metodos-array/tarefa08.js
@@ -0,0 +1,13 @@
+let tarefas = ['Estudar JavaScript', 'Fazer exercícios de matemática', 'Lavar a louça', 'Comprar mercado', 'Ir à academia', 'Ler um livro', 'Fazer revisão para o teste', 'Limpar o quarto', 'Estudar para concurso', 'Organizar o computador'];
+
+console.log("Antes do Splice:");
+console.table(tarefas);
+
+//Removendo a tarefa no índice 3 e substituimdo por uma nova tarefa
+let tarefasRemovidas = tarefas.splice(3, 1);
+
+console.log("Depois do Splice:");
+tarefas.splice(3,0, "Dar aula no SENAI");
+console.table(tarefas);
+
+console.log("As tarefas removidas foram:", tarefasRemovidas);
