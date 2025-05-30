53e446287443ce89a6e0d9f6cba07fa75bfece0e
isabeladuetes <isabela.duetes@aluno.senai.br>
2025-05-30 11:08:55 -0300

    feat: Ordena tarefas

---
+++ metodos-array/tarefa12.js
@@ -0,0 +1,6 @@
+let tarefas = ['Estudar JavaScript', 'Fazer exercícios de matemática', 'Lavar a louça', 'Comprar mercado', 'Ir à academia', 'Ler um livro', 'Fazer revisão para o teste', 'Limpar o quarto', 'Estudar para concurso', 'Organizar o computador'];
+
+//Organizando as tarefas em ordem alfabética
+let tarefasOrdenadas = tarefas.sort();
+
+console.log("Tarefas ordenadas:", tarefasOrdenadas);
