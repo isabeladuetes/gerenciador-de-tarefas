199ff4ddf0e01ded3cd37d9e15273539be8e9a14
isabeladuetes <isabela.duetes@aluno.senai.br>
2025-05-30 10:34:26 -0300

    feat: Concatena listas de tarefas

---
+++ metodos-array/tarefa09.js
@@ -0,0 +1,7 @@
+let tarefas1 = ['Estudar JavaScript', 'Fazer exercícios de matemática', 'Lavar a louça', 'Comprar mercado', 'Ir à academia', 'Ler um livro', 'Fazer revisão para o teste', 'Limpar o quarto', 'Estudar para concurso', 'Organizar o computador'];
+
+// Criando uma nova lista com tarefas adicionais
+let tarefas2 = ['Assistir TikTok', 'Postar vídeo no Insta'];
+
+//Juntando com a lista original
+console.log("Lista de tarefas combinadas:", tarefas1.concat(tarefas2));
